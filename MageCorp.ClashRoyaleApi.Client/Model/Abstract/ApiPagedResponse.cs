5ed126c9e10bc908c41b5cb13bd4f33835df2229
Rodrigo Bueno Piccelli <rodrigo.piccelli@gmail.com>
2024-07-12 00:49:51 -0300

    - Fixes an serious error on the paged lists deserialization - Updates System.Text.Json library because of a reported vulnerability

--- MageCorp.ClashRoyaleApi.Client/Model/Abstract/ApiPagedResponse.cs
+++ MageCorp.ClashRoyaleApi.Client/Model/Abstract/ApiPagedResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace MageCorp.ClashRoyaleApi.Client.Model.Abstract;
+﻿using System.Text.Json.Serialization;
+
+namespace MageCorp.ClashRoyaleApi.Client.Model.Abstract;
 
 /// <summary>
 /// </summary>
@@ -6,9 +8,11 @@ public abstract class ApiPagedResponse<T> : ApiResponse
 {
     /// <summary>
     /// </summary>
+    [JsonInclude]
     public List<T>? Items { get; internal set; }
     /// <summary>
     /// </summary>
+    [JsonInclude]
     public Paging? Paging { get; internal set; }
 
 }
