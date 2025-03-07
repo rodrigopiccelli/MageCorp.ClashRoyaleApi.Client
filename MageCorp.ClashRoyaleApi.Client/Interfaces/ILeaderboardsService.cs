acce4a7315896edf1e00cbf785b64ae8db1c522d
Rodrigo Bueno Piccelli <rodrigo.piccelli@gmail.com>
2025-03-07 05:11:33 -0300

    Update ILeaderboardsService.cs

--- MageCorp.ClashRoyaleApi.Client/Interfaces/ILeaderboardsService.cs
+++ MageCorp.ClashRoyaleApi.Client/Interfaces/ILeaderboardsService.cs
@@ -24,6 +24,7 @@ public interface ILeaderboardsService
     /// <param name="leaderboardId">A leaderboard ID from leaderboard API</param>
     /// <param name="limit">Limit the number of items returned in the response.</param>
     /// <param name="after">Return only items that occur after this marker. Before marker can be found from the response, inside the 'paging' property. Note that only after or before can be specified for a request, not both.</param>
+    /// <param name="before">Return only items that occur before this marker. Before marker can be found from the response, inside the 'paging' property. Note that only after or before can be specified for a request, not both.</param>
     /// <returns></returns>
     public Task<PlayerLeaderboardList> GetLeaderboardAsync(long leaderboardId, int? limit = null, string? after = null, string? before = null);
 
